6ceb875baf07325085ee8263cc727e2a16079ad4
Marker97 <chereche10@gmail.com>
2022-11-04 15:57:22 +0100

    Variables Methods Constructor_01

---
+++ src/it/develhope/house/House.java
@@ -0,0 +1,14 @@
+package it.develhope.house;
+
+public class House {
+    public String address;
+    public int numberOfFloors;
+
+    public House(String address, int numberOfFloors){
+        this.address = address;
+        this.numberOfFloors = numberOfFloors;
+        System.out.println("New house is created.. ");
+    }
+
+
+}
---
+++ src/it/develhope/house/Start.java
@@ -0,0 +1,9 @@
+package it.develhope.house;
+
+public class Start {
+    public static void main(String[] args) {
+        House house1, house2;
+        house1 = new House("Via balduccio,20", 2);
+        house2 = new House("Via dei Mille, 3", 3);
+    }
+}
